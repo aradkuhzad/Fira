148de0cfdc3ddabe206557a4881c2ca0e43c05cb
Arad <aradkuhzad@gmail.com>
2025-06-13 12:57:13 +0330

    Update index.html and styles.css: add smooth scrolling for navigation links, include new sections for cooking and about, and fix script tag in HTML.

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth scrolling for About
+document.querySelector('a[href="#about"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  const aboutSection = document.querySelector('#about');
+  const targetPosition = aboutSection.getBoundingClientRect().top + window.pageYOffset + 800;
+  
+  window.scrollTo({
+    top: targetPosition,
+    behavior: 'smooth',
+    duration: 3000
+  });
+});
+
+// Smooth scrolling for Cooking
+document.querySelector('a[href="#cooking"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  const background = document.querySelector('.background');
+  const backgroundHeight = background.offsetHeight;
+  const targetPosition = backgroundHeight / 0.9; 
+  
+  window.scrollTo({
+    top: targetPosition,
+    behavior: 'smooth',
+    duration: 3000
+  });
+}); 
